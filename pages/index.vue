7855737635904ec068419fc3de07e5a517880310
xiongxiong109 <444122662@qq.com>
2017-08-13 13:51:19 +0800

    init project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <router-link to="list">to list</router-link>
+  </section>
+</template>
+<script type="text/javascript">
+	export default {
+		head: {
+			title: 'home page',
+			meta: [
+				{name: 'keyword', content: 'nuxt work, vue, ssr'}
+			]
+		}
+	}
+</script>
---
+++ pages/list.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="com-page">
+		<h4>list page</h4>
+		<p>isDev: {{asyncMsg}}</p>
+		<p>msg: {{msg}}</p>
+	</div>
+</template>
+<script type="text/javascript">
+
+	export default {
+		data: () => {
+			return {
+				msg: 'ok'
+			}
+		},
+		asyncData: (ctx) => {
+			return {
+				asyncMsg: ctx.isDev
+			}
+		}
+	}
+</script>
