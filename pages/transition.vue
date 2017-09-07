5efe3f23e0d5fad039b2c6642c3c83d817a7a0d9
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-09-07 11:27:34 +0800

    add transition

---
+++ pages/transition.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="transition-test container">
+		<transition-group name="fade" mode="out-in">
+			<button @click="evt_change_toggle" v-if="!isOn" key="on">on</button>
+			<button @click="evt_change_toggle" v-else key="off">off</button>
+		</transition-group>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data: () => {
+			return {
+				isOn: true
+			}
+		},
+		methods: {
+			evt_change_toggle() {
+				this.isOn = !this.isOn
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	.transition-test {
+		position: relative;
+	}
+	button {
+		position: absolute;
+		top: 0;
+		left: 0;
+		opacity: 1;
+		transition: .3s ease;
+	}
+	.fade-enter-active {
+	  opacity: 0;
+	}
+	.fade-leave-active {
+	  opacity: 0;
+	}
+</style>
